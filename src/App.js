c1b6338cad2e8c2c9ac14cb71e7116818988df83
Brian Loveless <Brian.loveless1@yahoo.com>
2020-02-12 22:34:00 -0500

    mine now

--- src/App.js
+++ src/App.js
@@ -18,23 +18,26 @@ function App() {
   const { loading, error, data } = useSubscription(GET_PIXELS);
   const [color, changeColor] = useState("white");
   if (loading) {
-    return <div>loading ... just wait</div>;
+    return <div>loading ... just wait please it'll be drawing time soon ...</div>;
   }
   return (
-    <div className="content">
-      <div className="logo">Draw</div>
-      <p>Pick a Color</p>
-      <ColorPicker changeColor={changeColor} />
-      <p>Click a Pixel</p>
-      <div className="container">
-        {data.pixels.map((pixel, idx) => (
-          <Pixel
-            color={pixel.color}
-            id={pixel.id}
-            key={pixel.id}
-            newColor={color}
-          />
-        ))}
+    <div>
+      <h1 className="top"> Live web Sockets Pixel Art _-_ BWL</h1>
+      <div className="content">
+        <div className="logo">Draw</div>
+        <p>Pick a Color</p>
+        <ColorPicker changeColor={changeColor} />
+        <p>Click a Pixel</p>
+        <div className="container">
+          {data.pixels.map((pixel, idx) => (
+            <Pixel
+              color={pixel.color}
+              id={pixel.id}
+              key={pixel.id}
+              newColor={color}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
