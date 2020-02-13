4b9ff49ab9c861e0b22e8dee704a626f29d44677
Brian Loveless <Brian.loveless1@yahoo.com>
2020-02-12 22:17:47 -0500

    Updates

---
+++ precache-manifest.cdd21175d8a3539738939e259cdf2698.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6bb8a80dcf9a4fbfe8bd78ea21a4f7c",
+    "url": "/hasura-tutorial/index.html"
+  },
+  {
+    "revision": "c14a6cab5a2bdb1adae1",
+    "url": "/hasura-tutorial/static/css/main.06c2128c.chunk.css"
+  },
+  {
+    "revision": "276f38c6b27f53061de5",
+    "url": "/hasura-tutorial/static/js/2.ec726b24.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/hasura-tutorial/static/js/2.ec726b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c14a6cab5a2bdb1adae1",
+    "url": "/hasura-tutorial/static/js/main.aad92ae5.chunk.js"
+  },
+  {
+    "revision": "e04cf742af66e7b61a61",
+    "url": "/hasura-tutorial/static/js/runtime-main.b0f3db42.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio-improvements/precache-manifest.b9bd39cbb91f819cbdbf83f0ec933d59.js"
+  "/hasura-tutorial/precache-manifest.cdd21175d8a3539738939e259cdf2698.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio-improvements/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hasura-tutorial/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
