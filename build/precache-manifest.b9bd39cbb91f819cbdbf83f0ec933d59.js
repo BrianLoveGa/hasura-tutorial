2bd0db6c9a7916d6810036b59f5e8cd2f2be5fe2
Brian Loveless <Brian.loveless1@yahoo.com>
2020-02-12 22:09:53 -0500

    node modules

---
+++ build/precache-manifest.b9bd39cbb91f819cbdbf83f0ec933d59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c977bea20a019abcc74acdb787c7cfe",
+    "url": "/portfolio-improvements/index.html"
+  },
+  {
+    "revision": "9a58e77e445bc138aeec",
+    "url": "/portfolio-improvements/static/css/main.06c2128c.chunk.css"
+  },
+  {
+    "revision": "276f38c6b27f53061de5",
+    "url": "/portfolio-improvements/static/js/2.ec726b24.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/portfolio-improvements/static/js/2.ec726b24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a58e77e445bc138aeec",
+    "url": "/portfolio-improvements/static/js/main.4732102c.chunk.js"
+  },
+  {
+    "revision": "4e13fcbdee220a897bd1",
+    "url": "/portfolio-improvements/static/js/runtime-main.6a5b1235.js"
+  }
+]);
